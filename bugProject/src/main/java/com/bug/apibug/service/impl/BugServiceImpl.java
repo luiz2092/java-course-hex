835b76299dfe27af2ed5d6f33a77e202ab3f2f2a
Luiz Serrato <luiz.deco@hotmail.com>
2021-05-21 13:58:49 -0500

    add email reciver

--- bugProject/src/main/java/com/bug/apibug/service/impl/BugServiceImpl.java
+++ bugProject/src/main/java/com/bug/apibug/service/impl/BugServiceImpl.java
@@ -2,6 +2,7 @@ package com.bug.apibug.service.impl;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
 
 import com.bug.apibug.entity.BugEntity;
@@ -15,11 +16,19 @@ public class BugServiceImpl implements BugService {
 	@Autowired
 	private BugRepository bugRepository;
 
+	@Autowired
+	private JmsTemplate jmsTemplate;
+
 	@Override
 	public void createBug(BugModel bug) {
 		ModelMapper mapper = new ModelMapper();
 		BugEntity bugEntity = mapper.map(bug, BugEntity.class);
 		bugRepository.save(bugEntity);
+		queueMail(bug.getEmail());
+	}
+
+	private void queueMail(String email) {
+		jmsTemplate.convertAndSend("Send email to: ", email);
 	}
 
 }
---
+++ emailReciver/Receiver/src/main/java/com/ram/receiver/MessageReceiver.java
@@ -0,0 +1,21 @@
+package com.ram.receiver;
+
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+import com.ram.model.Product;
+
+@Component
+public class MessageReceiver {
+	private static final String MESSAGE_QUEUE = "message_queue";
+
+//	@JmsListener(destination = MESSAGE_QUEUE)
+//	public void receiveMessage(Product product) {
+//		System.out.println("Received " + product);
+//	}
+
+	@JmsListener(destination = "Send email to: ")
+	public void receiveMessage(String mail) {
+		System.out.println("Received " + mail);
+	}
+}
