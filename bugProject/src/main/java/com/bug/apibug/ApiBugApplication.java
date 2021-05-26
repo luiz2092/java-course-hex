2309ffdab374d5e2a976239642fe401ed8952adc
Luiz Serrato <luiz.deco@hotmail.com>
2021-05-26 09:25:05 -0500

    first push to dev

--- bugProject/src/main/java/com/bug/apibug/ApiBugApplication.java
+++ bugProject/src/main/java/com/bug/apibug/ApiBugApplication.java
@@ -2,6 +2,7 @@ package com.bug.apibug;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.jms.annotation.EnableJms;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -9,6 +10,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableJms
 @EnableSwagger2
+@EnableEurekaClient
 public class ApiBugApplication {
 
 	public static void main(String[] args) {
